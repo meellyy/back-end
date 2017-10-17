af0a1418a8832d0f8d98afacaa76bfc18d545547
Jaloliddin Bakirov <jaloliddinbakirov@Jaloliddins-MacBook-Pro.local>
2017-10-17 00:02:16 -0500

    deleted one line

--- Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
+++ Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
@@ -13,7 +13,6 @@ namespace Meellyy.TestingConsole
         	//asdasds
             Console.WriteLine("Hello World, this is development branch");
             Console.WriteLine("Hello World, this is Rusya");
-            Console.WriteLine("New push");
             Console.ReadLine();
         }
     }
