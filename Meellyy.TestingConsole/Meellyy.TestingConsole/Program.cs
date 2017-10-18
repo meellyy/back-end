fbc8992846acf42a82d74e6b6b77fd2ed5d75ec8
[Sh3R] <s_khojiakhmedov@SKHOJIAKHMEDOV->
2017-10-18 10:07:05 +0500

    testing1

--- Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
+++ Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
@@ -11,6 +11,9 @@ namespace Meellyy.TestingConsole
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            //GLobWeather.GlobalWeatherSoapClient client = new GLobWeather.GlobalWeatherSoapClient();
+            //var cities = client.GetCitiesByCountry("Uzbekistan");
+            Console.ReadLine();
             Console.ReadLine();
         }
     }
