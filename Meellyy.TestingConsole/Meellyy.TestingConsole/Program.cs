25f5e6bf24ee7baedc2593a78ad595bebc06b14a
[Sh3R] <s_khojiakhmedov@SKHOJIAKHMEDOV->
2017-10-19 17:47:58 +0500

    WCF and Entity DB Model created

--- Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
+++ Meellyy.TestingConsole/Meellyy.TestingConsole/Program.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using WcfServiceLibrary;
+using static WcfServiceLibrary.Service1;
 
 namespace Meellyy.TestingConsole
 {
@@ -10,15 +14,33 @@ namespace Meellyy.TestingConsole
     {
         public static void Main(string[] args)
         {
-        	//asdasds
-            Console.WriteLine("Hello World, this is development branch");
-            Console.WriteLine("Hello World, this is Rusya");
-            //GLobWeather.GlobalWeatherSoapClient client = new GLobWeather.GlobalWeatherSoapClient();
+            //GlobWeather.GlobalWeatherSoapClient client = new GlobWeather.GlobalWeatherSoapClient();
             //var cities = client.GetCitiesByCountry("Uzbekistan");
-            //Console.ReadLine();
-            //Console.WriteLine("Hello World");
-            GlobWeather.GlobalWeatherSoapClient client = new GlobWeather.GlobalWeatherSoapClient();
-            var cities = client.GetCitiesByCountry("Uzbekistan");
+            Uri baseAddress = new Uri("http://localhost:4567/GettingStarted/");
+            ServiceHost selfHost = new ServiceHost(typeof(DataRetriever), baseAddress);
+            try
+            {
+                //selfHost.AddServiceEndpoint(typeof(ICalculator), new WSHttpBinding(), "CalculatorService");
+                selfHost.AddServiceEndpoint(typeof(IRetrieveData), new WSHttpBinding(), "DataRetriever");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
             Console.ReadLine();
             //fuck github
         }
---
+++ Meellyy.TestingConsole/WcfServiceLibrary/IService1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceLibrary
+{
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+    }
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface ICalculator
+    {
+        [OperationContract]
+        double Add(double n1, double n2);
+    }
+    [ServiceContract(Namespace = "http://Microsoft.ServiceModel.Samples")]
+    public interface IRetrieveData
+    {
+        [OperationContract]
+        string retrieveFromTestTable();
+    }
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "WcfServiceLibrary.ContractType".
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
